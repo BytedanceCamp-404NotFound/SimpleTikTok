cf5c5aa7c4aed57846abf34c735d240ca82ecb0f
QAQ114514 <892492333@qq.com>
2023-01-20 18:02:58 +0800

    数据库连接优化：数据库连接文件与配置文件分离

--- oprations/sql/SqlConnect.go
+++ oprations/sql/SqlConnect.go
@@ -2,26 +2,40 @@ package sql
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 
+	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 /*
  *	函数功能：连接数据库
- *	输入参数 user:数据库账号
- *		     pwd:数据库密码
  *	返回值 *gorm.DB为链接上的数据库
  *  Tips:如果数据库参数不一致可以依照参数表注释修改
+ *  Tips:这里做了修改，将原本的配置信息放置在文件sqlConfig.yaml中
  */
-func SqlConnect(user string, pwd string) *gorm.DB {
+func SqlConnect() *gorm.DB {
 	//配置MySQL连接参数
-	username := user         //账号
-	password := pwd          //密码
-	host := "127.0.0.1"      //数据库地址，可以是Ip或者域名
-	port := 3306             //数据库端口
-	Dbname := "SimpleTikTok" //数据库名
-	timeout := "10s"         //连接超时，10秒
+	viper.SetConfigFile("oprations/sql/sqlConfig.yaml")
+	content, err := ioutil.ReadFile("oprations/sql/sqlConfig.yaml")
+	if err != nil {
+		fmt.Println("ioutil获取配置文件失败！")
+	}
+	err = viper.ReadConfig(strings.NewReader(os.ExpandEnv(string(content))))
+	if err != nil {
+		fmt.Println("viperhuoqu 配置文件失败！")
+	}
+	config := viper.Sub("database")
+
+	username := config.Get("username") //账号
+	password := config.Get("password") //密码
+	host := config.Get("host")         //数据库地址，可以是Ip或者域名
+	port := config.Get("port")         //数据库端口
+	Dbname := config.Get("dbname")     //数据库名
+	timeout := config.Get("timeout")   //连接超时，10秒
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local&timeout=%s", username, password, host, port, Dbname, timeout)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
