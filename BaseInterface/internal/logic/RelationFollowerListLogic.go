be6f3f82920321eb90815ae612e286d89034119d
XiaoKabuDa <8039581@qq.com>
2023-01-30 02:52:46 +0800

    fix:抽离3个Relation接口的mysql代码

--- BaseInterface/internal/logic/RelationFollowerListLogic.go
+++ BaseInterface/internal/logic/RelationFollowerListLogic.go
@@ -3,10 +3,10 @@ package logic
 import (
 	"SimpleTikTok/BaseInterface/internal/svc"
 	"SimpleTikTok/BaseInterface/internal/types"
+	"SimpleTikTok/oprations/commonerror"
 	"SimpleTikTok/oprations/mysqlconnect"
 	tools "SimpleTikTok/tools/token"
 	"context"
-	"fmt"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -27,39 +27,39 @@ func NewRelationFollowerListLogic(ctx context.Context, svcCtx *svc.ServiceContex
 
 func (l *RelationFollowerListLogic) RelationFollowerList(req *types.RelationFollowerListHandlerRequest) (resp *types.RelationFollowerListHandlerResponse, err error) {
 
-	testUserList_ := make([]types.RelationUser, 0)
-	tempUserList := types.RelationUser{}
-	resultJson := types.RelationFollowerListHandlerResponse{StatusCode: 501, StatusMsg: "token失效，请重新登录"}
-	result, TokenToUserID, err := tools.CheckToke(req.Token)
-	if !result {
-		return &resultJson, err
+	ok, id, err := tools.CheckToke(req.Token)
+	if !ok {
+		logx.Infof("[pkg]logic [func]PublishList [msg]req.Token is wrong ")
+		return &types.RelationFollowerListHandlerResponse{
+			StatusCode: int32(commonerror.CommonErr_PARSE_TOKEN_ERROR),
+			StatusMsg:  "登录过期，请重新登陆",
+		}, nil
 	}
-
-	db := mysqlconnect.GormDB
-	//#查找某个账号的粉丝列表   user_id：账号
-	userInfoList, err := db.Raw(fmt.Sprintf("SELECT * FROM user_info where user_id in(SELECT follower_id FROM follow_and_follower_list where user_id=%d)", req.UserId)).Rows()
-
 	if err != nil {
-		resultJson.StatusCode = 500
-		resultJson.StatusMsg = err.Error()
-	} else {
-		for userInfoList.Next() {
-			userInfoList.Scan(&tempUserList.Id, &tempUserList.Name, &tempUserList.FollowCount, &tempUserList.FollowerCount, &tempUserList.IsFollow) //！！err :查询出来的列数不同、数据格式不同时会报错，不影响格式正确的变量
-			testUserList_ = append(testUserList_, tempUserList)
-		}
-		//查询一遍上面查出来的id，是否已被当前登录的账号关注
-		for i := 0; i < len(testUserList_); i++ {
-			//是否已关注  follower_id：哪个账号想查询  user_id:哪个账号想被查询是否已被关注
-			sqlString := fmt.Sprintf("SELECT * FROM follow_and_follower_list where follower_id=%d and user_id=%d", TokenToUserID, testUserList_[i].Id)
-			if temtpp, _ := db.Raw(sqlString).Rows(); temtpp.Next() {
-				testUserList_[i].IsFollow = true
-			}
-		}
-		resultJson.StatusCode = 200
-		resultJson.StatusMsg = "查询成功"
+		logx.Errorf("[pkg]logic [func]PublishListr [msg]func CheckToken [err]%v", err)
+		return &types.RelationFollowerListHandlerResponse{
+			StatusCode: int32(commonerror.CommonErr_INTERNAL_ERROR),
+			StatusMsg:  "Token校验出错",
+		}, nil
 	}
+	resultJson := &types.RelationFollowerListHandlerResponse{}
 
-	resultJson.UserList = testUserList_
-	return &resultJson, err
+	// ！！！胡海龙：我先将sql代码抽离出来，有没有bug暂时没测试，过两天考试驾驶证再调试！！！
+
+	rflhr, err2 := mysqlconnect.GetFollowerList(int64(id), req.UserId)
+	Userlist := make([]types.RelationUser, 0)
+	for i := 0; i < len(rflhr); i++ {
+		Userlist[i] = types.RelationUser{
+			Id:            rflhr[i].Id,
+			Name:          rflhr[i].Name,
+			FollowCount:   rflhr[i].FollowCount,
+			FollowerCount: rflhr[i].FollowerCount,
+			IsFollow:      rflhr[i].IsFollow,
+		}
+		resultJson.StatusCode = 0
+		resultJson.StatusMsg = "success"
+	}
+	resultJson.UserList = Userlist
+	return resultJson, err2
 
 }
